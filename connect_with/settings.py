7bef39d00ee3a269b492bce4c2b0acf6ea906b0c
Tom-Nagy <tomnagy@hotmail.fr>
2022-06-18 14:11:27 +0000

    Change allowed host

--- connect_with/settings.py
+++ connect_with/settings.py
@@ -37,8 +37,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://connect-with.herokuapp.com/']
-CSRF_TRUSTED_ORIGINS = ['localhost', '127.0.0.1', 'https://connect-with.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'connect-with.herokuapp.com/']
+CSRF_TRUSTED_ORIGINS = ['localhost', '127.0.0.1', 'connect-with.herokuapp.com/']
 
 
 # Application definition
